9e6164f50e675028be0ae8d04aa67842cddcd9da
NelsonFonseca <nelsonfonseca80@gmail.com>
2021-03-10 21:12:00 -0500

    Movimiento

---
+++ TrabajoDeGrado/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //variables
+    public GameObject player;
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = obtiene la posicion de la camara o el objeeto
+        offset = transform.position - player.transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate() 
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
