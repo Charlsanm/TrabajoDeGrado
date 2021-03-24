812993c3f664c81aabdc1a8d798a4f6d30e30475
Carlos Mejia <mejia_98@hotmail.com>
2021-03-24 11:43:26 -0500

    Velocidad y Subir/Bajar Vehiculo
    Se añadió el componente de velocidad que muestre la velocidad actual del carro que se este manejando. Y por otro lado, se agregó la funcionalidad de bajar y subir del auto.

---
+++ TrabajoDeGrado/Assets/Scripts/GetOnTheCar.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GetOnTheCar : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject camPlayer;
+    public GameObject camCar;
+    private GameObject player;
+    private GameObject car;
+    private CarController scriptCarController;
+    private GetOutTheCar scriptGetOut;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        car = GameObject.FindGameObjectWithTag("Car");
+        scriptGetOut = car.GetComponent<GetOutTheCar>();
+
+        GameObject.Find("firetruck").GetComponent<CarController>().enabled = false;
+
+        text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.tag == "Player")
+        {
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other){
+        if (other.tag == "Player")
+        {
+            if (Input.GetButton("Y"))
+            {
+                text.SetActive(false);
+                camPlayer.SetActive(false);
+                camCar.SetActive(true);
+                //Destroy(player);
+                player.SetActive(false);
+                car.tag = "Player";
+                GameObject.Find("firetruck").GetComponent<CarController>().enabled = true;
+                scriptGetOut.enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (other.tag == "Player")
+        {
+            text.SetActive(false);
+        }
+    }
+}
